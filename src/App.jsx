1acb6b6f011c137606b3c0507440e6b41bfca6c3
SaikatKarar <kararsaikat@gmail.com>
2024-06-06 11:50:22 +0530

    Header is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+
 function App() {
   return (
     <div className='App'>
-      <h1 className="text-red-500">Welcome to nextPage!</h1>
+      <Header />
+      <Outlet />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About';
+import Book from './components/Book'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            }, {
+                path: "/books",
+                element: <Book />
+            },
+            {
+                path: 'about',
+                element: <About />,
+            }
+        ]
+    },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+)
